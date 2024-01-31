fbe6a4487a4bee85ab64d6b0e149504a0bfe306d
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-01-30 22:39:17 -0800

    math

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use std::io;
 use rand::Rng;
 use std::io::{Write, BufReader, BufRead, ErrorKind};
@@ -5,7 +7,28 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    data_types()
+}
+
+fn math_operators() {
+    let is_true = true;
+    let my_grade = 'A';
+
+    let num_1: f32 = 1.111111111111111;
+    println!("f32 : {}", num_1 + 0.111111111111111);
+
+    let num_2: f64 = 1.111111111111111;
+    println!("f64 : {}", num_2 + 0.111111111111111);
+
+    let num_3: u32 = 5;
+    let num_4: u32 = 4;
+    println!("5 + 4 = {}", num_3 + num_4);
+    println!("5 - 4 = {}", num_3 - num_4);
+    println!("5 * 4 = {}", num_3 * num_4);
+    println!("5 / 4 = {}", num_3 / num_4);
+    println!("5 % 4 = {}", num_3 % num_4);
+
+    let random_num = rand::thread_rng().gen_range(1..101);
+    println!("Random number: {}", random_num);
 }
 
 fn data_types() {
